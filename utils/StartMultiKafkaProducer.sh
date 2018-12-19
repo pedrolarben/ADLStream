d60bc8211ac2cc35efc8918f507d32923ac6fcb9
Pedro larben <pedrolarabenitez@gmail.com>
2018-12-19 18:42:07 +0100

    small changes

--- utils/StartMultiKafkaProducer.sh
+++ utils/StartMultiKafkaProducer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../projects/kafkapy/MultiARFFProducer.py --bootstrap_servers 10.141.0.225:1025
+python ../projects/kafkapy/MultiARFFProducer.py --bootstrap_servers localhost:9092
