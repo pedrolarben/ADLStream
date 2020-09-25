d331af3868a11c24037ef033d0608b5d343cabf3
pedrolarben <pedrolarabenitez@gmail.com>
2020-09-25 23:36:33 +0200

    Add missing requirements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ LONG_DESC_TYPE = "text/markdown"
 INSTALL_REQUIRES = [
     "numpy",
     "tensorflow>=2.1.0",
-    "tensorflow-addons",
+    "tensorflow-addons>=0.11.0",
     "keras-tcn",
     "matplotlib",
     "scikit-learn",
