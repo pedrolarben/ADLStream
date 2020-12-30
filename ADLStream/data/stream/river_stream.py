f0d262443910b919c2210217fe7a0de62e8d8449
pedrolarben <pedrolarabenitez@gmail.com>
2020-12-30 13:35:38 +0100

    Improve river stream wrapper

--- ADLStream/data/stream/river_stream.py
+++ ADLStream/data/stream/river_stream.py
@@ -37,13 +37,21 @@ class RiverStream(BaseStream):
             Defaults to 30000.
     """
 
-    def __init__(self, dataset, stream_period=0, timeout=30000, **kwargs):
+    def __init__(
+        self, dataset, stream_period=0, timeout=30000, n_instances=1000, **kwargs
+    ):
         super().__init__(stream_period=stream_period, timeout=timeout, **kwargs)
         self.dataset = dataset
+        self.n_instances = n_instances
+        self.data = None
+
+    def start(self):
+        self.data = self.dataset.take(self.n_instances)
+        super().start()
 
     def get_message(self):
         # Get X and y from river stream
-        X, y = list(self.dataset.take(1))[0]
+        X, y = next(self.data)
         # Convert X from dict format to list
         X = list(X.values())
         # Return X and y concatenated as a single list
