5ebb387eb045f6c130490b9990503e01f1dd7674
pedrolarben <pedrolarabenitez@gmail.com>
2021-02-18 11:25:04 +0000

    New release 0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 PACKAGE_NAME = "ADLStream"
 AUTHOR = "Pedro Lara-Benítez & Manuel Carranza-García"
 AUTHOR_EMAIL = "plbenitez@us.es"
