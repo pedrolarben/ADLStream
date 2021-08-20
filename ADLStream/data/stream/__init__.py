52b7b576b34da0ae48ef064174fbd81da8cc6395
pedrolarben <pedrolarabenitez@gmail.com>
2021-08-21 00:24:47 +0100

    Add DataStream

--- ADLStream/data/stream/__init__.py
+++ ADLStream/data/stream/__init__.py
@@ -4,3 +4,4 @@ from ADLStream.data.stream.csv_file_stream import CSVFileStream
 from ADLStream.data.stream.kafka_stream import KafkaStream
 from ADLStream.data.stream.fake_stream import FakeStream
 from ADLStream.data.stream.river_stream import RiverStream
+from ADLStream.data.stream.data_stream import DataStream
